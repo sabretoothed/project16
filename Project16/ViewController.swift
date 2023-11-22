07e83a1f7cdeed1d8b618151219a2af3df3a73b3
sabretoothed <sabretoothed.sqr@gmail.com>
2023-11-22 19:24:34 +0400

    test 2

--- Project16/ViewController.swift
+++ Project16/ViewController.swift
@@ -13,6 +13,8 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         view.backgroundColor = .white
+        let label = UILabel()
+        label.text = "Привет"
     }
 
 

d34531621382a37fa00441a42d137d177f849e5d
sabretoothed <sabretoothed.sqr@gmail.com>
2023-11-22 19:22:29 +0400

    test 1

---
+++ Project16/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Project16
+//
+//  Created by dmitry malik on 22.11.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        view.backgroundColor = .white
+    }
+
+
+}
+
